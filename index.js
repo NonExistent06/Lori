da2662b7a588a5bfbac4e2f1301266db53418c9d
NonExistent <75103662+NonExistent06@users.noreply.github.com>
2020-12-08 17:44:36 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const config = require('./config.json')
+const command = require('./command')
+
+client.on('ready', () => {
+  console.log('The client is ready!')
+
+  command(client, ['ping', 'test'], (message) => {
+    message.channel.send('Pong!')
+  })
+
+  command(client, 'servers', (message) => {
+    client.guilds.cache.forEach((guild) => {
+      message.channel.send(
+        `${guild.name} has a total of ${guild.memberCount} members`
+      )
+    })
+  })
+
+  command(client, ['cc', 'clearchannel'], (message) => {
+    if (message.member.hasPermission('ADMINISTRATOR')) {
+      message.channel.messages.fetch().then((results) => {
+        message.channel.bulkDelete(results)
+      })
+    }
+  })
+
+  command(client, 'status', (message) => {
+    const content = message.content.replace('>status ', '')
+    // "!status hello world" -> "hello world"
+
+    client.user.setPresence({
+      activity: {
+        name: content,
+        type: 0,
+      },
+    })
+  })
+})
+
+command(client, 'ban', (message) => {
+    const { member, mentions } = message
+
+    const tag = `<@${member.id}>`
+
+    if (
+      member.hasPermission('ADMINISTRATOR') ||
+      member.hasPermission('BAN_MEMBERS')
+    ) {
+      const target = mentions.users.first()
+      if (target) {
+        const targetMember = message.guild.members.cache.get(target.id)
+        targetMember.ban()
+        message.channel.send(`${tag} That user has been banned.`)
+      } else {
+        message.channel.send(`${tag} Please specify someone to ban.`)
+      }
+    } else {
+      message.channel.send(
+        `${tag} You do not have permission to use this command.`
+      )
+    }
+  })
+
+  command(client, 'kick', (message) => {
+    const { member, mentions } = message
+
+    const tag = `<@${member.id}>`
+
+    if (
+      member.hasPermission('ADMINISTRATOR') ||
+      member.hasPermission('KICK_MEMBERS')
+    ) {
+      const target = mentions.users.first()
+      if (target) {
+        const targetMember = message.guild.members.cache.get(target.id)
+        targetMember.kick()
+        message.channel.send(`${tag} That user has been kicked`)
+      } else {
+        message.channel.send(`${tag} Please specify someone to kick.`)
+      }
+    } else {
+      message.channel.send(
+        `${tag} You do not have permission to use this command.`
+      )
+    }
+  })
+
+client.login(config.token)
+
